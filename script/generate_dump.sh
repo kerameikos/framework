da6ec986a90904b81acf232c1fd7e4eb1647cb43
Ethan Gruber <ewg4xuva@gmail.com>
2015-02-08 12:40:50 -0500

    updated dump scripts

--- script/generate_dump.sh
+++ script/generate_dump.sh
@@ -1,7 +1,14 @@
-#generate RDF data dumps
-cd /usr/local/projects/ceramics
+#Generate dumps
+cd /usr/local/projects/kerameikos
 echo "Generating RDF/XML."
-OUTPUT=dump/kerameikos.org.xml
-java -jar script/saxon9.jar config.xml script/generate_rdfxml.xsl > $OUTPUT
+java -jar script/saxon9.jar -s:config.xml -xsl:ui/xslt/apis/aggregate-all.xsl -o:dump/kerameikos.org.rdf
+echo "Done."
+#use wrapper to generate TTL
+echo "Generating Turtle."
+rapper -i rdfxml -o turtle dump/kerameikos.org.rdf > dump/kerameikos.org.ttl
+echo "Done."
+#use saxon to create JSON-LD
+echo "Generating JSON-LD."
+java -jar script/saxon9.jar -s:dump/kerameikos.org.rdf -xsl:ui/xslt/serializations/rdf/json-ld.xsl -o:dump/kerameikos.org.jsonld
 echo "Done."
 echo "Process complete."
