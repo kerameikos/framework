56ddff0772fafe602479581210cbbeb68387d1f9
Ethan Gruber <ewg4xuva@gmail.com>
2021-04-26 11:18:08 -0400

    updated JS to use the index of the canvas as a label if there is no label property

--- ui/javascript/display_functions.js
+++ ui/javascript/display_functions.js
@@ -131,8 +131,13 @@ $(document).ready(function () {
                 };
                 
                 // For each image create a L.TileLayer.Iiif object and add that to an object literal for the layer control
-                $.each(data.sequences[0].canvases, function (_, val) {
-                    iiifLayers[val.label] = L.tileLayer.iiif(val.images[0].resource.service[ '@id'] + '/info.json');
+                $.each(data.sequences[0].canvases, function (_, val) {  
+                    if (val.hasOwnProperty('label')) {
+                        var label = val.label;
+                    } else {
+                        var label = _;
+                    }
+                    iiifLayers[label] = L.tileLayer.iiif(val.images[0].resource.service[ '@id'] + '/info.json');
                 });
                 // Add layers control to the map
                 L.control.layers(iiifLayers).addTo(iiifImage);
